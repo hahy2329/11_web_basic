b38b6148d4a8a97b3771dc50677a2c05dbcfcc79
12_web_hjs <12_web_hjs@DESKTOP-2RF42K3>
2023-03-24 14:40:04 +0900

    최초 커밋

---
+++ 11_web_basic/WebContent/03_javascript/chapter01_basic/example.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+
+document.write("<h1>외부 자바스크립트</h1>");
+document.write("<p>import해서 사용합니다.</p>");
+
+
+
+console.log("외부 자바스크립트 테스트1");
+console.log("외부 자바스크립트 테스트2");
+console.log("외부 자바스크립트 테스트3");
+
+
+alert("반갑습니다.");
